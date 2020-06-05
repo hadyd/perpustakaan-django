bdf4c453539d3de2bf948c349c4a1e7b2913b245
hadyd <anr.hadyd@gmail.com>
2020-06-05 18:31:01 +0700

    First Init

---
+++ README.md
@@ -0,0 +1,11 @@
+## Belajar Framework Django - Python
+### Membuat Project Perpustakaan Sederhana
+
+- Belajar Basic Routing Django
+- Belajar Template Django
+- Belajar Setup Bootstrap Django
+- Belajar Setup Database
+- Belajar Models
+- Belajar Django Admin
+- Belajar Model Form
+- Belajar CRUD: Menambah Data 
---
+++ perpustakaan/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+from perpustakaan.models import Buku
+from perpustakaan.forms import FormBuku
+
+def buku(request):
+    books = Buku.objects.all()
+
+    konteks = {
+        'books': books,
+    }
+    return render(request, 'buku.html', konteks)
+
+def penerbit(request):
+    return render(request, 'penerbit.html')
+
+def tambah_buku(request):
+    if request.POST:
+        form = FormBuku(request.POST)
+        if form.is_valid():
+            form.save()
+            form = FormBuku()
+            pesan = "Data Buku Berhasil disimpan"
+
+            konteks = {
+                'form': form,
+                'pesan': pesan,
+            }
+            return render(request, 'tambah-buku.html', konteks)
+    else:
+        form = FormBuku()
+
+        konteks = {
+            'form': form,
+        }
+
+    return render(request, 'tambah-buku.html', konteks)
